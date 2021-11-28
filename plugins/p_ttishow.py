33196ee0573d00ea8da7ce26d6291e7d144751d1
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-11-28 12:08:38 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,7 +1,10 @@
+import random
+import asyncio
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT
+from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, VID
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from utils import get_size, temp
