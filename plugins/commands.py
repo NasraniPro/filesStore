45c33b0c3a1a5f626f7fd4dd50e3aaff8b2deff7
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-01 02:12:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -136,7 +136,7 @@ async def start(client, message):
         parse_mode="markdown"
         )
     
-   except Exception as err:
+ 
    await message.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
    elif len(message.command) > 1 and message.command[1] == 'subscribe':
        invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
