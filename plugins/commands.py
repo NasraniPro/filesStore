dfa4c6fde7531dd31826d904ac72d2c654c06246
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-01 01:36:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,6 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
             sticker=random.choice(STICKER),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -89,7 +88,6 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
             sticker=random.choice(STICKER),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
