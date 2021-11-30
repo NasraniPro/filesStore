aeb8b7a468a456f8ec2b30daf87268850d8bb536
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-11-30 23:53:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,18 +110,17 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-    btn = [
-            [
-                InlineKeyboardButton(
-                    "📩𝐉𝐨𝐢𝐧 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥📩", url=invite_link.invite_link
-                )
-            ]
-        ]
-    await client.send_cached_media(
-        chat_id=message.from_user.id,
-        file_id=file_id,
-        caption=f_caption,
-        )
+    buttons = [
+                    [
+                        InlineKeyboardButton('💌 SUBSCRIBE ✅', url=https://t.me/bigmoviesworld')
+                    ]
+                    ]
+                await bot.send_cached_media(
+                    chat_id=message.from_user.id,
+                    file_id=file_id,
+                    caption=f_caption,
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                    )
                     
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
