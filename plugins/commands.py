c8e75a0b19b9ba682e7b2379f87ad932d596c614
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-01 01:13:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -117,8 +117,9 @@ async def start(client, message):
     if f_caption is None:
         f_caption = f"{files.file_name}"
     buttons = [
+                    [
                         InlineKeyboardButton('💌 SUBSCRIBE ✅', url='https://t.me/bigmoviesworld')
-                    ]
+                    ],
                     [
                         InlineKeyboardButton('💌 SUBSCRIBE ✅', url='https://t.me/bigmoviesworld'),
                         InlineKeyboardButton('💌 SUBSCRIBE ✅', url='https://t.me/bigmoviesworld')
