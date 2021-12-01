31a2e6c6766cf338c6e0825871678f6181082dd8
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-01 13:08:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,7 +59,7 @@ async def not_joined(client: Client, message: Message):
 
 
 
-
+   else:
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
