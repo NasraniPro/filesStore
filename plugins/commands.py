bec05c480f395681408382474a346d215ef2c4c7
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-01 13:47:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -122,7 +122,7 @@ async def start(client, message):
         f_caption = f"{files.file_name}"
     buttons = [
                     [
-                        InlineKeyboardButton('💌 SUBSCRIBE ✅', url=f'https://telegram.me/share/url?url={link}')
+                        InlineKeyboardButton('💌 SUBSCRIBE ✅', url=f'https://telegram.me/share/url?url={file_link}')
                     ],
                     [
                         InlineKeyboardButton('💌 SUBSCRIBE ✅', url='https://t.me/bigmoviesworld'),
