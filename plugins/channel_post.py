ce4919e40b3f128531cf23c9d0ce08aec5dcb549
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-20 12:55:30 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
-from bot import Bot
+from b import Bot
 
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
